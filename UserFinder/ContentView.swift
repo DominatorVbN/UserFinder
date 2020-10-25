5a4250ddf26025707d438b01fc4d676a170dea97
DominatorVbN <voilentbynature@gmail.com>
2020-10-25 13:33:06 +0530

    Initial Commit

---
+++ UserFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserFinder
+//
+//  Created by Amit Samant on 25/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserFinder/UserFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserFinderApp.swift
+//  UserFinder
+//
+//  Created by Amit Samant on 25/10/20.
+//
+
+import SwiftUI
+
+@main
+struct UserFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
